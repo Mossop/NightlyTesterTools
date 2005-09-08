9b173eb2439cc86999a00c2beac51b9d5e91ecbb
dtownsend <dtownsend@oxymoronical.com>
2005-09-08 10:06:03 +0000

    Moved to a better model for extension installation. One global service handles a count and displays a prompt when all installs are complete.

--- chrome/nightly/content/xpinstall.js
+++ chrome/nightly/content/xpinstall.js
@@ -10,49 +10,13 @@ var NightlyXPInstall = {
 
 startDownload: function(name,url)
 {
-	var directoryService = Components.classes["@mozilla.org/file/directory_service;1"].
-										getService(Components.interfaces.nsIProperties);
-	dir = directoryService.get("TmpD",Components.interfaces.nsIFile);
-
-	var i=0;
-	var file;
-	do
-	{
-		file=dir.clone();
-		file.append("nightly-"+i+".xpi");
-		if (!file.exists())
-		{
-			file.create(Components.interfaces.nsILocalFile.NORMAL_FILE_TYPE, 0644);
-			break;
-		}
-		i++
-	} while (i<1000);
-	if (i<1000)
-	{
-	  var ioService = Components.classes["@mozilla.org/network/io-service;1"]
-	                            .getService(Components.interfaces.nsIIOService);
-	  fileuri=ioService.newFileURI(file);
-	  xpiuri=ioService.newURI(url,null,null);
-		
-		var persist = Components.classes["@mozilla.org/embedding/browser/nsWebBrowserPersist;1"]
-											.createInstance(Components.interfaces.nsIWebBrowserPersist);
-		const nsIWBP = Components.interfaces.nsIWebBrowserPersist;
-		const flags = nsIWBP.PERSIST_FLAGS_REPLACE_EXISTING_FILES;
-		persist.persistFlags = flags | nsIWBP.PERSIST_FLAGS_FROM_CACHE;
-		persist.persistFlags |= nsIWBP.PERSIST_FLAGS_AUTODETECT_APPLY_CONVERSION;
-	
-		// Create download and initiate it (below)
-		var tr = Components.classes["@mozilla.org/transfer;1"].createInstance(Components.interfaces.nsITransfer);
-	  tr.init(xpiuri, fileuri, name, null, null, null, persist);
-	  var listener = Components.classes["@blueprintit.co.uk/downloadlistener;1"]
-	  											.createInstance(Components.interfaces.nsIDownloadListener);
-	  listener.init(file,tr);
-	  persist.progressListener = listener;
-	  persist.saveURI(xpiuri, null, null, null, null, fileuri);
-	}
-	else
-	{
-	}
+  var ioService = Components.classes["@mozilla.org/network/io-service;1"]
+                            .getService(Components.interfaces.nsIIOService);
+  xpiuri=ioService.newURI(url,null,null);
+  
+  var nightlyService = Components.classes["@blueprintit.co.uk/nightlytools;1"]
+                            .getService(Components.interfaces.nsINightlyToolsService);
+  nightlyService.installExtension(name,xpiuri);
 },
 
 accept: function()
--- components/nsDownloadListener.js
+++ components/nsDownloadListener.js
@@ -10,8 +10,9 @@ function DownloadListener()
 {
 }
 
-DownloadListener.prototype.init = function(file,transfer)
+DownloadListener.prototype.init = function(name,file,transfer)
 {
+  this.name=name;
 	this.file=file;
 	this.transfer=transfer;
 }
@@ -36,164 +37,10 @@ DownloadListener.prototype.onStateChange = function(webProgress, request, stateF
 	this.transfer.onStateChange(webProgress, request, stateFlags, status);
 	if (stateFlags&Components.interfaces.nsIWebProgressListener.STATE_STOP)
 	{
-		try
-		{
-			var directoryService = Components.classes["@mozilla.org/file/directory_service;1"].
-												getService(Components.interfaces.nsIProperties);
-			dir = directoryService.get("TmpD",Components.interfaces.nsIFile);
-		
-			var i=0;
-			var rdffile;
-			do
-			{
-				rdffile=dir.clone();
-				rdffile.append("nightly-rdf-"+i+".rdf");
-				if (!rdffile.exists())
-				{
-					rdffile.create(Components.interfaces.nsILocalFile.NORMAL_FILE_TYPE, 0644);
-					break;
-				}
-				i++
-			} while (i<1000);
-			
-			if (i<1000)
-			{
-				var zipReader = Components.classes["@mozilla.org/libjar/zip-reader;1"]
-										.createInstance(Components.interfaces.nsIZipReader);
-				zipReader.init(this.file);
-				zipReader.open();
-				zipReader.extract("install.rdf", rdffile);
-				
-			  var ioService = Components.classes["@mozilla.org/network/io-service;1"]
-			                            .getService(Components.interfaces.nsIIOService);
-			  fileuri=ioService.newFileURI(rdffile);
-
-				rdfService = Components.classes["@mozilla.org/rdf/rdf-service;1"]
-													.getService(Components.interfaces.nsIRDFService);
-				var ds = rdfService.GetDataSourceBlocking(fileuri.spec);
-				var source = rdfService.GetResource("urn:mozilla:install-manifest");
-				var idprop = rdfService.GetResource("http://www.mozilla.org/2004/em-rdf#id");
-				var id = ds.GetTarget(source,idprop,true);
-				if (id)
-				{
-					id=id.QueryInterface(Components.interfaces.nsIRDFLiteral);
-					extensionID=id.Value;
-					rdffile.remove(false);
-					
-					var em = Components.classes["@mozilla.org/extensions/manager;1"]
-											.getService(Components.interfaces.nsIExtensionManager);
-					var installLocation = em.getInstallLocation(extensionID);
-					if (!installLocation)
-					{
-						installLocation = em.getInstallLocation("{8620c15f-30dc-4dba-a131-7c5d20cf4a29}");
-					}
-					var dest = installLocation.getItemLocation(extensionID);
-					
-					try
-					{
-			      // create directories first
-			      var entries = zipReader.findEntries("*/");
-			      while (entries.hasMoreElements())
-			      {
-			        var entry = entries.getNext().QueryInterface(Components.interfaces.nsIZipEntry);
-			        var target = installLocation.getItemFile(extensionID, entry.name);
-			        if (!target.exists())
-			        {
-			          try
-			          {
-			            target.create(Components.interfaces.nsILocalFile.DIRECTORY_TYPE, 0755);
-			          }
-			          catch (e)
-			          {
-			          }
-			        }
-			      }
-			
-			      entries = zipReader.findEntries("*");
-			      while (entries.hasMoreElements())
-			      {
-			        entry = entries.getNext().QueryInterface(Components.interfaces.nsIZipEntry);
-			        target = installLocation.getItemFile(extensionID, entry.name);
-			        try
-			        {
-				        if (!target.exists())
-				          target.create(Components.interfaces.nsILocalFile.NORMAL_FILE_TYPE, 0644);
-				        zipReader.extract(entry.name, target);
-			        }
-			        catch (e)
-			        {
-			        }
-			      }
-					}
-					catch (e)
-					{
-						dump("Failed - "+e+"\n");
-					}
-	
-					var appinfo = Components.classes['@mozilla.org/xre/app-info;1']
-																.getService(Components.interfaces.nsIXULAppInfo);
-					var appid = appinfo.ID;
-					var appversion = appinfo.version;
-					try
-					{
-						appversion=prefservice.getCharPref("app.extensions.version");
-						if (!appversion)
-							appversion=appinfo.version;
-					}
-					catch (e) { }
-					var versionliteral = rdfService.GetLiteral(appversion);
-					
-					var vc = Components.classes["@mozilla.org/xpcom/version-comparator;1"]
-	                                 .getService(Components.interfaces.nsIVersionComparator);
-		
-					manifest=ioService.newFileURI(installLocation.getItemFile(extensionID, "install.rdf"));
-					ds = rdfService.GetDataSourceBlocking(manifest.spec);
-					var targappprop = rdfService.GetResource("http://www.mozilla.org/2004/em-rdf#targetApplication");
-					var minprop = rdfService.GetResource("http://www.mozilla.org/2004/em-rdf#minVersion");
-					var maxprop = rdfService.GetResource("http://www.mozilla.org/2004/em-rdf#maxVersion");
-					
-					var changed=false;
-					var apps = ds.GetTargets(source,targappprop,true);
-					while (apps.hasMoreElements())
-					{
-						var appentry = apps.getNext();
-						var id = ds.GetTarget(appentry,idprop,true);
-						if (id)
-						{
-							id=id.QueryInterface(Components.interfaces.nsIRDFLiteral);
-							if (id.Value==appid)
-							{
-								var minv = ds.GetTarget(appentry,minprop,true).QueryInterface(Components.interfaces.nsIRDFLiteral);
-								var maxv = ds.GetTarget(appentry,maxprop,true).QueryInterface(Components.interfaces.nsIRDFLiteral);
-								
-								if (vc.compare(appversion,minv.Value)<0)
-								{
-									ds.Change(appentry,minprop,minv,versionliteral);
-									changed=true;
-								}
-								
-								if (vc.compare(appversion,maxv.Value)>0)
-								{
-									ds.Change(appentry,maxprop,maxv,versionliteral);
-									changed=true;
-								}
-							}
-						}
-					}
-					if (changed)
-					{
-			      ds.QueryInterface(Components.interfaces.nsIRDFRemoteDataSource);
-			      ds.Flush();
-					}
-				}
-				zipReader.close();
-			}
-		}
-		catch (e)
-		{
-			dump("Failed - "+e+"\n");
-		}
-		this.file.remove(false);
+    var nightlyService = Components.classes["@blueprintit.co.uk/nightlytools;1"]
+                              .getService(Components.interfaces.nsINightlyToolsService);
+    nightlyService.installLocalExtension(this.name,this.file);
+  	this.file.remove(false);
 	}
 }
 
