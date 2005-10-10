e1f9cd12416836ef55a0eceb4bed7f967b5d1069
dtownsend <dtownsend@oxymoronical.com>
2005-10-10 15:51:57 +0000

    Fixed incompatibility with Mr Tech Local Install

--- chrome/nightly/content/nightly.js
+++ chrome/nightly/content/nightly.js
@@ -203,7 +203,7 @@ init: function()
 	nightly.preferences.addObserver("",nightly,false);
 	nightly.prefChange("idtitle");
 
-	nightly.versionCheck('0.7.9.7');	
+	nightly.versionCheck('0.7.9.8');	
 },
 
 prefChange: function(pref)
