e08e0e8954380ca1929f88f15f75cdb35d9a040e
dtownsend <dtownsend@oxymoronical.com>
2006-03-16 22:07:11 +0000

    Update for new talkback url

--- src/defaults/preferences/nightlytools.js
+++ src/defaults/preferences/nightlytools.js
@@ -12,5 +12,5 @@ pref("nightly.leaks.filterLeaked", true);
 pref("nightly.leaks.filterCollected", true);
 pref("nightly.leaks.filterIgnored", true);
 
-pref("nightly.talkback.searchurl", "http://talkback-public.mozilla.org/talkback/fastfind.jsp?search=2&type=iid&id=");
+pref("nightly.talkback.searchurl", "http://talkback-public.mozilla.org/search/start.jsp?search=2&type=iid&id=");
 pref("extensions.{8620c15f-30dc-4dba-a131-7c5d20cf4a29}.description", "chrome://nightly/locale/nightly.properties");
